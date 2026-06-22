0e45873cfb1d44f2a978332b0976165cc861b141
Mily_0 <browserbma@gmail.com>
2026-06-22 14:35:16 -0300

    Move KnownBugs to DEVELOPER and add dev notes
    Rename KnownBugs.toml into DEVELOPER/KnownBugs.toml and update BLURRED_TEXT: set estado to true and replace the long diagnostic comments with a concise suggestion to install DirectX (Windows) or OpenGL (Linux). Add a new developer file DEVELOPER/StuffthatNEEDtobeAdded.hx containing planned ideas and templates (sound path/playSound example and a template placeholder) for future features.
    
    copinto

---
+++ DEVELOPER/StuffthatNEEDtobeAdded.hx
@@ -0,0 +1,33 @@
+/**
+    * Diferente dos bugs, aqui estarão ideias projetadas para lançar eventualmente!!!!
+    * Coisas planejadas para a V1, ou posteriores.
+
+    * ----------------------------------------------------------------------
+    * Fucking Love my chud HaxeFlixel life!!! 🍭🍭😋😋
+    * ----------------------------------------------------------------------
+*/
+
+
+// * [PLAYSOUND_PATHS]
+var PLAYSOUND_PATHS = {
+    idea: "
+        a ideia seria bem mais simples doq parece,
+        só simplesmente fazer uma outra string no PlaySound
+        que mostraria o folder em que se encontra.
+        exemplo no comentário abaixo:
+    "
+};
+
+    // playSound(Path:String, FileName:String, ?Volume:Float, ?Tag:String);
+    //
+    // Haxe já faz isso usando dos Paths pré feitos com General/Game/Editors e tudo mais.
+    // Porém usando o sistema de Paths seria mais fáceis.
+
+    // playSound('game/secretsHAHA', 'secret.ogg', 1, 'secreto');
+
+// * [TEMPLATE]
+var TEMPLATE = {
+    idea: "
+        template??? O QUEEEE/?!?!?!?!?
+    "
+};
