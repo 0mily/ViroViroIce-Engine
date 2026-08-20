bd62800d72eced657e54d83b6b6290405aaa79a3
Mily_0 <135561818+0mily@users.noreply.github.com>
2026-08-20 17:09:05 -0300

    Organize and update ClientPrefs variables
    Refactor ClientPrefs to organize variables into categories and add new gameplay settings.

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -11,44 +11,78 @@ using StringTools;
 // Add a variable here and it will get automatically saved
 @:structInit class SaveVariables {
 
-// ViroViroIce
-
-	public var mechanics:Bool = true;
-	public var modchart:Bool = true;
-	public var weekpixel:Bool = true;
-	public var mikudside:Bool = false;
-	public var extra:Bool = false;
-	public var developerMode:Bool = false;
-	public var neatRating:Bool = true;
-	public var neatWindow:Float = 22.0;
-	public var sustainAlpha:Float = 1;
-	public var opaqueSustains:Bool = true; // isso não deveria ser strums ao invés de sustains?? -Shiho
-	public var multithreadedLoading:Bool = #if (desktop || cpp || hl) true #else false #end;
-	public var screenshots:Bool = true;
-	public var screenshotKey:Dynamic = {keyboard: 'F3', gamepad: 'NONE'}; // dps eu faço melhor // ah eu // eu deixei f3 ao invés de f10 porque é assim no pedaço-v -Shiho
-
-// Psych
-
+// ======================[Gameplay]
+	
 	public var downScroll:Bool = false;
 	public var middleScroll:Bool = false;
-	public var opponentStrums:Bool = true;
-	public var showFPS:Bool = true;
-	public var flashing:Bool = true;
-	public var autoPause:Bool = true;
-	public var antialiasing:Bool = true;
+	
+	public var mechanics:Bool = true;
+	public var modchart:Bool = true;
+	public var ghostTapping:Bool = true;
+	public var safeFrames:Float = 10.0;
+	public var guitarHeroSustains:Bool = true;
+	public var noteOffset:Int = 0;
+	public var ratingOffset:Int = 0;
+	public var neatRating:Bool = true;
+
+	public var noReset:Bool = false;
+		
+	public var neatWindow:Float = 22.0;
+	public var sickWindow:Float = 45.0;
+	public var goodWindow:Float = 90.0;
+	public var badWindow:Float = 135.0;
+
+	
+// ======================[Visuals]
+	
 	public var noteSkin:String = 'Default';
 	public var splashSkin:String = 'ViroViroIce';
 	public var holdSkin:String = 'Vanilla';
+	
+	public var opaqueSustains:Bool = true; // isso não deveria ser strums ao invés de sustains?? -Shiho // sla - Mily
 	public var splashAlpha:Float = 0.8;
 	public var holdSplashAlpha:Float = 1;
-	public var lowQuality:Bool = false;
-	public var shaders:Bool = true;
-	public var cacheOnGPU:Bool = #if !switch false #else true #end; // GPU Caching made by Raltyro
-	public var framerate:Int = 60;
-	public var unlockedFPS:Bool = false;
+	public var sustainAlpha:Float = 1;
+
+	public var healthBarAlpha:Float = 1;
 	public var camZooms:Bool = true;
 	public var hideHud:Bool = false;
-	public var noteOffset:Int = 0;
+	public var scoreZoom:Bool = true;
+
+	public var weekpixel:Bool = true;
+
+
+// ======================[Graphics]
+	public var lowQuality:Bool = false;
+	public var antialiasing:Bool = true;
+	public var shaders:Bool = true;
+	public var cacheOnGPU:Bool = #if !switch false #else true #end; // GPU Caching made by Raltyro
+
+	public var framerate:Int = 60;
+	public var unlockedFPS:Bool = false;
+
+	
+// ======================[QoL]
+
+	public var flashing:Bool = true;
+	public var autoPause:Bool = true;
+	public var comboStacking:Bool = true;
+	public var multithreadedLoading:Bool = #if (desktop || cpp || hl) true #else false #end;
+	public var screenshots:Bool = true;
+	public var pauseMusic:String = 'Tea Time';
+	public var hitsoundVolume:Float = 0;
+	
+	
+// ======================[Misc.]
+
+	public var showFPS:Bool = true; // change to showDEBUG pls - Mily
+	public var developerMode:Bool = false;
+	public var discordRPC:Bool = true;
+	public var checkForUpdates:Bool = true;
+
+
+// ======================[Too Specific to organize]
+
 	public var arrowRGB:Array<Array<FlxColor>> = [
 		[0xFFC24B99, 0xFFFFFFFF, 0xFF3C1F56],
 		[0xFF00FFFF, 0xFFFFFFFF, 0xFF1542B7],
@@ -60,28 +94,24 @@ using StringTools;
 		[0xFF71E300, 0xFFF6FFE6, 0xFF003100],
 		[0xFFFF884E, 0xFFFFFAF5, 0xFF6C0000]];
 
-	public var ghostTapping:Bool = true;
+	public var language:String = 'en-US';
+	public var loadingScreen:Bool = true;
+	public var comboOffset:Array<Int> = [0, 0, 0, 0];
+	
+
+// ======================[Unused or just fucked up]
+
+	public var mikudside:Bool = false;
+	public var extra:Bool = false;
+	public var opponentStrums:Bool = true; // prob not making this an option on Options? It's quite useless.
 	public var timeBarType:String = 'Time Elapsed';
-	public var scoreZoom:Bool = true;
-	public var noReset:Bool = false;
-	public var healthBarAlpha:Float = 1;
-	public var hitsoundVolume:Float = 0;
-	public var pauseMusic:String = 'Tea Time';
-	public var checkForUpdates:Bool = true;
-	public var comboStacking:Bool = true;
+	public var screenshotKey:Dynamic = {keyboard: 'F3', gamepad: 'NONE'}; // lets just make this public var a key on controls? - Mily
+
+// ======================[MODIFIERS]
+
 	public var gameplaySettings:Map<String, Dynamic> = [
 		'scrollspeed' => 1.0,
-		'scrolltype' => 'multiplicative', 
-		// anyone reading this, amod is multiplicative speed mod, cmod is constant speed mod, and xmod is bpm based speed mod.
-		// an amod example would be chartSpeed * multiplier
-		// cmod would just be constantSpeed = chartSpeed
-		// and xmod basically works by basing the speed on the bpm.
-		// iirc (beatsPerSecond * (conductorToNoteDifference / 1000)) * noteSize (110 or something like that depending on it, prolly just use note.height)
-		// bps is calculated by bpm / 60
-		// oh yeah and you'd have to actually convert the difference to seconds which I already do, because this is based on beats and stuff. but it should work
-		// just fine. but I wont implement it because I don't know how you handle sustains and other stuff like that.
-		// oh yeah when you calculate the bps divide it by the songSpeed or rate because it wont scroll correctly when speeds exist.
-		// -kade
+		'scrolltype' => 'multiplicative', // whatever you say, Kade! We love you! - Mily
 		'songspeed' => 1.0,
 		'healthgain' => 1.0,
 		'healthloss' => 1.0,
@@ -91,24 +121,18 @@ using StringTools;
 		'opponentplay' => false
 	];
 
-	public var comboOffset:Array<Int> = [0, 0, 0, 0];
-	public var ratingOffset:Int = 0;
-	public var sickWindow:Float = 45.0;
-	public var goodWindow:Float = 90.0;
-	public var badWindow:Float = 135.0;
-	public var safeFrames:Float = 10.0;
-	public var guitarHeroSustains:Bool = true;
-	public var discordRPC:Bool = true;
-	public var loadingScreen:Bool = true;
-	public var language:String = 'en-US';
-
+// ======================[Editors]
+	
 	public var editorSFX:Bool = true;
 	public var chartEditorMusic:Bool = true;
 	public var chartEditorMusicVolume:Float = 0.35;
 	public var chartEditorLegacyAutosave:Bool = false;
 	public var chartEditorDefaultConverter:String = '';
 	public var chartEditorDefaultConverterDifficulty:String = 'normal';
+	
 }
+
+// ======================[Resto!!!]
 
 class ClientPrefs {
 	public static inline final UNLOCKED_FPS_CAP:Int = 1000;
@@ -133,6 +157,7 @@ class ClientPrefs {
 		'back'			=> [BACKSPACE, ESCAPE],
 		'pause'			=> [ENTER, ESCAPE],
 		'reset'			=> [R],
+		'screenshot'	=> [F3],
 		
 		'volume_mute'	=> [ZERO],
 		'volume_up'		=> [NUMPADPLUS, PLUS],
@@ -156,6 +181,7 @@ class ClientPrefs {
 		'back'			=> [B],
 		'pause'			=> [START],
 		'reset'			=> [BACK]
+		'screenshot'    => [RIGHT_ANALOG_STICK] // ?
 	];
 	public static var defaultKeys:Map<String, Array<FlxKey>> = null;
 	public static var defaultButtons:Map<String, Array<FlxGamepadInputID>> = null;
