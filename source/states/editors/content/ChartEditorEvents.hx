f2184cb6d90ae9f8f4b53b8594380cdcddd7d4d8
Mily_0 <135561818+0mily@users.noreply.github.com>
2026-08-21 01:23:09 -0300

    Implement ChartEditorEvents class with methods
    doing it rn just to start my evil plan

--- source/states/editors/content/ChartEditorEvents.hx
+++ source/states/editors/content/ChartEditorEvents.hx
@@ -6,4 +6,28 @@ import states.editors.ChartingState;
 @:access(states.editors.ChartingState) // ur dur
 class ChartEditorEvents
 {
+  public var editor(default, null):ChartingState;
+	public var tab(default, null):FlxSpriteGroup;
+
+  public function new(editor:ChartingState, tab:FlxSpriteGroup)
+	{
+		this.editor = editor;
+		this.tab = tab;
+	}
+
+  // called once per chart editor framee!
+	public function update(elapsed:Float):Void {}
+
+  // reloads the availabel event defination and ther editor metadata
+	public function reload():Void {}
+
+  // rebulds the events tab from the current chart selection
+	public function onSelectionChanged():Void {}
+
+  // builds da data stored by a newly placed event
+	public function buildEventData():Array<String>
+	{
+		return ['', '', '']; // o vazio do HaxeFlixel
+	}
+  
 }
