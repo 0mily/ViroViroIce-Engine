793cc5e6d9907b984cca72055538baab496c5df1
Mily_0 <135561818+0mily@users.noreply.github.com>
2026-08-21 01:16:08 -0300

    bruh

--- source/states/editors/content/ChartEditorEvents.hx
+++ source/states/editors/content/ChartEditorEvents.hx
@@ -3,7 +3,7 @@ package states.editors.content;
 import flixel.group.FlxSpriteGroup;
 import states.editors.ChartingState;
 
-
+@:access(states.editors.ChartingState) // ur dur
 class ChartEditorEvents
 {
 }
